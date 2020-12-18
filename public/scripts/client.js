b21a2915d8fa5fa867e5a673a4d4c63cb7e3d8c2
Kelechi Obikaonu <kelechipauline@gmail.com>
2020-12-18 01:58:46 +0000

    refactored code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,67 +1,47 @@
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-
-
-
-
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-  const renderTweets = function(tweets) {
-    for (const tweet of tweets) {
-      const $htmltweet = createTweetElement(tweet);
-      $('#tweets-container').prepend($htmltweet)
-    }
-  };
-
-
-
-  //Convert JSON date to normal date and get time since tweet was madee3cx
-   
-  const timesince = function(jsonDate) {
-    const sec = Math.floor((new Date() - jsonDate) / 1000);
-    let interval = Math.floor(sec / 31536000)
-    if (interval > 1) {
-      return interval + 'years';
-    }
-    interval = Math.floor(sec / 2592000);
-    if (interval > 1) {
-      return interval + " months";
-    }
-    interval = Math.floor(sec / 86400);
-    if (interval > 1) {
-      return interval + " days";
-    }
-    interval = Math.floor(sec / 3600);
-    if (interval > 1) {
-      return interval + " hours";
-    }
-    interval = Math.floor(sec / 60);
-    if (interval > 1) {
-      return interval + " minutes";
-    }
-    return Math.floor(sec) + " seconds";
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const $htmltweet = createTweetElement(tweet);
+    $('#tweets-container').prepend($htmltweet);
   }
+};
+
+//Convert JSON date to normal date and get time since tweet was madee3cx
+const timesince = function(jsonDate) {
+  const sec = Math.floor((new Date() - jsonDate) / 1000);
+  let interval = Math.floor(sec / 31536000);
+  if (interval > 1) {
+    return interval + 'years';
+  }
+  interval = Math.floor(sec / 2592000);
+  if (interval > 1) {
+    return interval + " months";
+  }
+  interval = Math.floor(sec / 86400);
+  if (interval > 1) {
+    return interval + " days";
+  }
+  interval = Math.floor(sec / 3600);
+  if (interval > 1) {
+    return interval + " hours";
+  }
+  interval = Math.floor(sec / 60);
+  if (interval > 1) {
+    return interval + " minutes";
+  }
+  return Math.floor(sec) + " seconds";
+};
 
 
-  const createTweetElement = function(tweetData) {
-
-    // convert data to safe data
-    const safeHTML = `<p>${escape(tweetData['content'].text)}</p>`
-   // using jquery to create a DOM object
-   const tweetArticle = `<article class="tweet"> 
+const createTweetElement = function(tweetData) {
+  // convert data to safe data
+  const safeHTML = `<p>${escape(tweetData['content'].text)}</p>`;
+  // using jquery to create a DOM object
+  const tweetArticle = `<article class="tweet"> 
     <header>
       <div class="tweet-icon">
       <img class="icon" src=${tweetData['user'].avatars}>
@@ -85,53 +65,51 @@ const tweetData = {
   </article>`;
 
   return tweetArticle;
- };
+};
  
- //handle XSS edge case
- const escape =  function(str) {
+//handle XSS edge case
+const escape =  function(str) {
   let p = document.createElement('p');
   p.appendChild(document.createTextNode(str));
   return p.innerHTML;
-}
+};
 
- // AJAX POST for submit button
+// AJAX POST for submit button
  
- $(function() {
-  const $form = $("#newtweet")
-  $form.submit(function(event) {
+$(function() {
+  const $form = $("#newtweet");
+  $form.submit(function(event)  {
     event.preventDefault();
-    const tweetcontent = $(this).children("#tweet-text")
+    const tweetcontent = $(this).children("#tweet-text");
     if (!tweetcontent.val() || tweetcontent.val() === null) {
-      $("#error").html("!!! Whats Tweeter without a tweet?")
-      $("#error").addClass("errormessage")
+      $("#error").html("!!! Whats Tweeter without a tweet?");
+      $("#error").addClass("errormessage");
     } else if (tweetcontent.val().length > 140) {
-      $("#error").html("!!! Tweet too long")
-      $("#error").addClass("errormessage")
-    }
-    else {
-      $("#error").html("")
-      $("#error").removeClass("errormessage")
+      $("#error").html("!!! Tweet too long");
+      $("#error").addClass("errormessage");
+    } else {
+      $("#error").html("");
+      $("#error").removeClass("errormessage");
       $.ajax({
         method: 'POST',
         url: "/tweets",
         data: $(this).serialize()
       }).then(res => {
         $("textarea").val("");
-        loadTweets()
-      })
+        loadTweets();
+        $("#counter").val(140)
+      });
     }
   });
 
   // function to load tweets from the server
   const loadTweets = function() {
     $.getJSON("/tweets", function(data) {
-      console.log('success',data)
-    })
-    .then(function (tweetdata) {
-      renderTweets(tweetdata)
-    })
+    }).then(function (tweetdata) {
+      renderTweets(tweetdata);
+    });
   };
-  loadTweets()
- });
+  loadTweets();
+});
  
  
